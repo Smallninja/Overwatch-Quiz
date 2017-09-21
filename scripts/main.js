de73268e5d9f987b477af0b2d6eafebbead8efb3
Joseph Gutierrez <smallninja.jay@gmail.com>
2017-09-20 21:48:08 -0500

    Now keeps track of score and clears radio buttons when clicked.

--- scripts/main.js
+++ scripts/main.js
@@ -12,7 +12,10 @@
 	var answerC = document.querySelector('#answerC');
 	var answerD = document.querySelector('#answerD');
 
-	var questionPlace = 1;
+	var allInput = document.getElementsByClassName("css-checkbox");
+
+	var questionPlace = 0;
+	var score = 0;
 
 	var Question = function(h1, firstVal, secondVal, thirdVal, fourthVal){
 		this.h1 = h1;
@@ -22,13 +25,27 @@
 		this.fourthVal = fourthVal;
 	}
 		
-	var firstQuestion = new Question("Test Question1", ["A", 0], ["B", 0], ["C", 0], ["D", 1]);
-	var secondQuestion = new Question("Test Question2", ["A", 0], ["B", 0], ["C", 1], ["D", 0]);
-	var thirdQuestion = new Question("Test Question3", ["A", 0], ["B", 1], ["C", 0], ["D", 0]);
-	var fourthQuestion = new Question("Test Question4", ["A", 1], ["B", 0], ["C", 0], ["D", 0]);
+	var firstQuestion = new Question("Test Question1", ["A", false], ["B", false], ["C", false], ["D", true]);
+	var secondQuestion = new Question("Test Question2", ["A", false], ["B", false], ["C", true], ["D", false]);
+	var thirdQuestion = new Question("Test Question3", ["A", false], ["B", true], ["C", false], ["D", false]);
+	var fourthQuestion = new Question("Test Question4", ["A", true], ["B", false], ["C", false], ["D", false]);
 
+    function checkAnswer() {
+        for (var i = 0; i < allInput.length; i++) {
+            if (allInput[i].checked === true) {
+                if (allInput[i].value == "true") {
+                    score++;
+                    allInput[i].checked = false
+                }
+                else{
+                	allInput[i].checked = false;
+                }
+            }
+        }
+    }
 
 	function changeQuestion(object){
+
 		question.innerHTML = object.h1;
 
 		optionA.innerHTML = object.firstVal[0];
@@ -48,20 +65,27 @@
 
 	submitBtn.addEventListener('click', function(e){
 		e.preventDefault();
-		if(questionPlace === 2 ){
+		if(questionPlace === 1 ){
+			checkAnswer();
 			changeQuestion(secondQuestion);
 			return;
 		}
-		if(questionPlace === 3){
+		if(questionPlace === 2){
+			checkAnswer();
 			changeQuestion(thirdQuestion);
 			return;
 		}
-		if(questionPlace === 4){
+		if(questionPlace === 3){
+			checkAnswer();
 			changeQuestion(fourthQuestion);
+			return;
+		}
+		if(questionPlace === 4){
+			checkAnswer();
+			console.log(score);
 			return;
 		}
 	});
 
 	changeQuestion(firstQuestion);
-
 })(window);
